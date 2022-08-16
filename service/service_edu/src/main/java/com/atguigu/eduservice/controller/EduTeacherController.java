fd0e21691f352f34fdbe004828e4090b114e0530
xiaobailer <th15986581153@gmail.com>
2022-08-16 11:06:04 +0800

    day01完成分页添加修改异常处理

--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -1,19 +1,20 @@
 package com.atguigu.eduservice.controller;
 
 
+import com.atguigu.baserservice.handler.GuliException;
 import com.atguigu.commonutils.R;
 import com.atguigu.eduservice.entity.EduTeacher;
+import com.atguigu.eduservice.entity.vo.TeacherQuery;
 import com.atguigu.eduservice.service.EduTeacherService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang3.StringUtils;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -40,11 +41,16 @@ public class EduTeacherController {
     @GetMapping
     public R getAllTeacher() {
         List<EduTeacher> list = eduTeacherService.list(null);
+        try {
+            int i = 10 / 0;
+        } catch (Exception e) {
+            throw new GuliException(20001, "自定义异常");
+        }
         return R.ok().data("list", list);
     }
 
     @ApiOperation(value = "删除教师")
-    @GetMapping("{id}")
+    @DeleteMapping("{id}")
     public R delTeacher(@PathVariable String id) {
         boolean remove = eduTeacherService.removeById(id);
         if (remove) {
@@ -59,12 +65,76 @@ public class EduTeacherController {
     @GetMapping("selectByPage/{current}/{limit}")
     public R selectByPage(@PathVariable Long current, @PathVariable Long limit) {
         Page<EduTeacher> eduTeacherPage = new Page<>(current, limit);
+        IPage<EduTeacher> page = eduTeacherService.page(eduTeacherPage, null);
         long total = eduTeacherPage.getTotal();
         List<EduTeacher> records = eduTeacherPage.getRecords();
         Map<String, Object> map = new HashMap<>();
         map.put("list", records);
         map.put("total", total);
         return R.ok().data(map);
+    }
+
+    @ApiOperation(value = "待条件分页查询教师列表")
+    @PostMapping("selectByPageVo/{current}/{limit}")
+    public R selectByPageVo(@PathVariable Long current, @PathVariable Long limit, @RequestBody TeacherQuery teacherQuery) {
+        //@RequesyBody 把json串转换成实体类
+        //1.取出查询条件
+        String name = teacherQuery.getName();
+        Integer level = teacherQuery.getLevel();
+        String begin = teacherQuery.getBegin();
+        String end = teacherQuery.getEnd();
+        //2.判断条件是否为空，如果不空拼写sql
+        QueryWrapper<EduTeacher> queryWrapper = new QueryWrapper<>();
+        if (!StringUtils.isEmpty(name)) {
+            queryWrapper.like("name", name);
+        }
+        if (level != null) {
+            queryWrapper.eq("level", level);
+        }
+        if (!StringUtils.isEmpty(begin)) {
+            queryWrapper.ge("gmt_create", begin);
+        }
+        if (!StringUtils.isEmpty(end)) {
+            queryWrapper.le("gmt_create", end);
+        }
+        Page<EduTeacher> eduTeacherPage = new Page<>(current, limit);
+        eduTeacherService.page(eduTeacherPage, queryWrapper);
+        long total = eduTeacherPage.getTotal();
+        List<EduTeacher> records = eduTeacherPage.getRecords();
+        Map<String, Object> map = new HashMap<>();
+        map.put("list", records);
+        map.put("total", total);
+        return R.ok().data(map);
+    }
+
+    @ApiOperation(value = "添加讲师")
+    @PostMapping("addTeacher")
+    public R addTeacher(@RequestBody EduTeacher eduTeacher) {
+        boolean save = eduTeacherService.save(eduTeacher);
+        if (save) {
+            return R.ok();
+        } else {
+            return R.error();
+        }
+
+    }
+
+    @ApiOperation(value = "根据id查询讲师")
+    @GetMapping("selectTeacherById/{id}")
+    public R selectTeacherById(@PathVariable String id) {
+        EduTeacher teacher = eduTeacherService.getById(id);
+        return R.ok().data("eduTeacher", teacher);
+    }
+
+    @ApiOperation(value = "修改讲师")
+    @PostMapping("updateTeacher")
+    public R updateTeacher(@RequestBody EduTeacher eduTeacher) {
+        boolean b = eduTeacherService.updateById(eduTeacher);
+        if (b) {
+            return R.ok();
+        } else {
+            return R.error();
+        }
     }
 }
 
--- service/service_edu/src/main/java/com/atguigu/eduservice/entity/EduTeacher.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/entity/EduTeacher.java
@@ -1,11 +1,11 @@
 package com.atguigu.eduservice.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.*;
+
 import java.util.Date;
-import com.baomidou.mybatisplus.annotation.TableId;
+
 import java.io.Serializable;
 
-import com.baomidou.mybatisplus.annotation.TableLogic;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -54,9 +54,11 @@ public class EduTeacher implements Serializable {
     @ApiModelProperty(value = "逻辑删除 1（true）已删除， 0（false）未删除")
     private Boolean isDeleted;
 
+    @TableField(fill = FieldFill.INSERT)
     @ApiModelProperty(value = "创建时间")
     private Date gmtCreate;
 
+    @TableField(fill = FieldFill.UPDATE)
     @ApiModelProperty(value = "更新时间")
     private Date gmtModified;
 
