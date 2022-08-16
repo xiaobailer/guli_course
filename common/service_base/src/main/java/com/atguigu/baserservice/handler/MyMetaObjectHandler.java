31fb31f5a2ecf7fdcbb704834bbc82e045a3651b
xiaobailer <th15986581153@gmail.com>
2022-08-16 11:06:51 +0800

    day01完成分页添加修改异常处理

---
+++ common/service_base/src/main/java/com/atguigu/baserservice/handler/MyMetaObjectHandler.java
@@ -0,0 +1,20 @@
+package com.atguigu.baserservice.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.setFieldValByName("gmtCreate", new Date(), metaObject);
+        this.setFieldValByName("gmtModified", new Date(), metaObject);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+
+    }
+}
