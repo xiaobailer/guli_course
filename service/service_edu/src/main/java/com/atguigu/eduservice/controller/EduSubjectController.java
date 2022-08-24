d8b30444085f76b4297658047f96b020082aa720
xiaobailer <th15986581153@gmail.com>
2022-08-24 20:18:59 +0800

    day04完成了课程列表展现

--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduSubjectController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduSubjectController.java
@@ -3,15 +3,16 @@ package com.atguigu.eduservice.controller;
 
 import com.atguigu.commonutils.R;
 import com.atguigu.eduservice.entity.EduSubject;
+import com.atguigu.eduservice.entity.vo.OneSubjectVo;
 import com.atguigu.eduservice.service.EduSubjectService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 /**
  * <p>
@@ -24,6 +25,7 @@ import org.springframework.web.multipart.MultipartFile;
 @Api(description = "课程分类管理")
 @RestController
 @RequestMapping("/eduservice/edu-subject")
+@CrossOrigin // 解决跨域问题
 public class EduSubjectController {
     @Autowired
     private EduSubjectService eduSubjectService;
@@ -34,6 +36,14 @@ public class EduSubjectController {
         //手动传入service
         eduSubjectService.addSubject(file,eduSubjectService);
         return R.ok();
+    }
+
+
+    @ApiOperation(value = "查询所有讲师")
+    @GetMapping("getAllSubject")
+    public R getAllSubject() {
+        List<OneSubjectVo> allSubject =  eduSubjectService.getAllSubject();
+        return R.ok().data("allSubject", allSubject);
     }
 }
 
